5ef49757f5ae10856278f9e6cafe74017a3f5f0b
leey1111 <c92662972@gmail.com>
2024-06-10 12:39:48 +0900

    #8 게시판 글쓰기 - 컨트롤러 완료/전체 작업 끝

---
+++ SpringBoard/src/main/java/com/itwillbs/controller/BoardController.java
@@ -0,0 +1,49 @@
+package com.itwillbs.controller;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.itwillbs.domain.BoardVO;
+import com.itwillbs.service.BoardService;
+
+@Controller
+@RequestMapping(value = "/board/*")
+public class BoardController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
+	// 서비스 객체 주입
+	@Inject
+	private BoardService bService;
+	
+	// 게시판 글쓰기 - get
+	@RequestMapping(value = "/regist", method = RequestMethod.GET)
+	public void registGET() throws Exception{
+		logger.debug("게시판 글쓰기 get - 사용자의 정보 입력");
+		logger.debug("연결된 view페이지 이동");
+	}
+	
+	// 게시판 글쓰기 - post
+	@RequestMapping(value = "/regist", method = RequestMethod.POST)
+	public String registPOST(BoardVO vo) throws Exception{
+		logger.debug("게시판 글쓰기 POST - 입력된 데이터 처리");
+		
+		// 한글처리 인코딩(필터 처리)
+		// 전달정보 저장
+		logger.debug("vo : "+ vo);
+		
+		// 서비스-> DAO에 동작 호출
+		bService.regist(vo);
+		
+		// 페이지 이동
+		return "redirect:/board/list";
+	}
+	
+
+}
